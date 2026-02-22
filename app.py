34ff1a7c7cef09c59f959d94dd91ba020dc78dcc
Shubham Sattigeri <sattigeri.shubham@outlook.com>
2026-02-22 15:34:52 +0530

    Fix Import for Deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from .run_pipeline import run_pipeline
+from run_pipeline import run_pipeline
 
 app = FastAPI()
 
